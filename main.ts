bf3931ed83ab71c7d7c3ed89b4fa270dbb86c31a
nic-youthacademy <109627731+nic-youthacademy@users.noreply.github.com>
2022-07-19 15:33:38 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,15 @@
+let column = 0
+let row = 0
+basic.showIcon(IconNames.Ghost)
+basic.forever(function () {
+    for (let index = 0; index < 4; index++) {
+        if (input.soundLevel() > 128) {
+            row = randint(0, 4)
+            column = randint(0, 4)
+        }
+        if (led.point(column, row)) {
+            led.unplot(column, row)
+            led.plot(column + 1, row)
+        }
+    }
+})
